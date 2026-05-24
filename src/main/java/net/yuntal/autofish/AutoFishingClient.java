5998f5e82484d04660ab5d1eac54e72be0dec752
Yoon Hari <bringmeback4444@gmail.com>
2026-05-24 21:59:07 +0900

    fix: added checks for rod, tweaked reel logic

--- src/main/java/net/yuntal/autofish/AutoFishingClient.java
+++ src/main/java/net/yuntal/autofish/AutoFishingClient.java
@@ -5,12 +5,15 @@ import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 public class AutoFishingClient implements ClientModInitializer {
     public static final String MOD_ID = "auto-fishing";
 
     private int recastTimer = -1;
     private int castCooldown = 0;
+    private int inWaterTicks = 0;
     private boolean hookWasNull = true;
 
     @Override
@@ -19,6 +22,15 @@ public class AutoFishingClient implements ClientModInitializer {
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player == null || client.level == null) return;
+
+            ItemStack heldItem = client.player.getItemInHand(InteractionHand.MAIN_HAND);
+            if (heldItem.getItem() != Items.FISHING_ROD) {
+                recastTimer = -1;
+                castCooldown = 0;
+                inWaterTicks = 0;
+                hookWasNull = true;
+                return;
+            }
 
             AutoFishConfig config = AutoConfig.getConfigHolder(AutoFishConfig.class).getConfig();
 
@@ -36,12 +48,15 @@ public class AutoFishingClient implements ClientModInitializer {
 
             if (hook == null) {
                 hookWasNull = true;
+                inWaterTicks = 0;
                 return;
             }
 
+            // new cast
             if (hookWasNull) {
                 hookWasNull = false;
                 castCooldown = config.castCooldown;
+                inWaterTicks = 0;
             }
 
             if (castCooldown > 0) {
@@ -49,9 +64,17 @@ public class AutoFishingClient implements ClientModInitializer {
                 return;
             }
 
-            if (hook.isInWater() && hook.getDeltaMovement().y < config.biteThreshold) {
+            if (hook.isInWater()) {
+                inWaterTicks++;
+            }
+
+            if (inWaterTicks < 10) return;
+
+
+            if (hook.getDeltaMovement().y < config.biteThreshold) {
                 client.gameMode.useItem(client.player, InteractionHand.MAIN_HAND);
                 recastTimer = config.recastDelay;
+                inWaterTicks = 0;
             }
         });
     }
